4727a2e6794c9b8e650f6385d5555d7914c471c7
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2025-01-12 18:53:44 +0330

    remove inline keyword from state functions

--- compose-plus/functions/src/commonMain/kotlin/ir/ehsannarmani/compose_plus/functions/state/SaveableState.kt
+++ compose-plus/functions/src/commonMain/kotlin/ir/ehsannarmani/compose_plus/functions/state/SaveableState.kt
@@ -13,47 +13,47 @@ import androidx.compose.runtime.saveable.autoSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
-inline fun <T>rememberSaveableState(value:T,saver: Saver<T, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) {
+fun <T>rememberSaveableState(value:T,saver: Saver<T, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) {
     mutableStateOf(value)
 }
 @Composable
-inline fun <T>rememberSaveableState(value:T,saver: Saver<T, out Any> = autoSaver(),vararg keys:Any) = rememberSaveable(*keys, stateSaver = saver) {
+fun <T>rememberSaveableState(value:T,saver: Saver<T, out Any> = autoSaver(),vararg keys:Any) = rememberSaveable(*keys, stateSaver = saver) {
     mutableStateOf(value)
 }
 
 @Composable
-inline fun <T>rememberSaveableListState(vararg items:T,saver: Saver<List<T>, out Any> = autoSaver()) = rememberSaveable(saver = saver) {
+fun <T>rememberSaveableListState(vararg items:T,saver: Saver<List<T>, out Any> = autoSaver()) = rememberSaveable(saver = saver) {
     mutableStateListOf(*items)
 }
 @Composable
-inline fun <reified T>rememberSaveableListState(vararg items:T,keys:List<Any> = emptyList(),saver: Saver<List<T>, out Any> = autoSaver()) = rememberSaveable(*keys.toTypedArray(),saver = saver) {
+fun <T>rememberSaveableListState(vararg items:T,keys:List<Any> = emptyList(),saver: Saver<List<T>, out Any> = autoSaver()) = rememberSaveable(*keys.toTypedArray(),saver = saver) {
     mutableStateListOf(*items)
 }
 
 @Composable
-inline fun <reified T>rememberSaveableListState(items:List<T>,saver: Saver<List<T>, out Any> = autoSaver()) = rememberListState(*items.toTypedArray(),saver)
+inline fun <reified T>rememberSaveableListState(items:List<T>, saver: Saver<List<T>, out Any> = autoSaver()) = rememberListState(*items.toTypedArray(),saver)
 @Composable
-inline fun <reified T>rememberSaveableListState(items:List<T>,saver: Saver<List<T>, out Any> = autoSaver(),vararg keys: Any) = rememberListState(*items.toTypedArray(),saver,keys)
+inline fun <reified T>rememberSaveableListState(items:List<T>, saver: Saver<List<T>, out Any> = autoSaver(), vararg keys: Any) = rememberListState(*items.toTypedArray(),saver,keys)
 
 @Composable
-inline fun <K,V>rememberSaveableMapState(vararg pairs:Pair<K,V>,saver: Saver<Map<K,V>, out Any> = autoSaver()) = rememberSaveable(saver = saver) {
+fun <K,V>rememberSaveableMapState(vararg pairs:Pair<K,V>,saver: Saver<Map<K,V>, out Any> = autoSaver()) = rememberSaveable(saver = saver) {
     mutableStateMapOf(*pairs)
 }
 @Composable
-inline fun <K,V>rememberSaveableMapState(vararg pairs:Pair<K,V>,keys: List<Any>,saver: Saver<Map<K,V>, out Any> = autoSaver()) = rememberSaveable(*keys.toTypedArray(),saver) {
+fun <K,V>rememberSaveableMapState(vararg pairs:Pair<K,V>,keys: List<Any>,saver: Saver<Map<K,V>, out Any> = autoSaver()) = rememberSaveable(*keys.toTypedArray(),saver) {
     mutableStateMapOf(*pairs)
 }
 
 @Composable
-inline fun rememberSaveableBoolState(initial:Boolean = false,saver: Saver<Boolean, out Any> = autoSaver()) = rememberSaveableState(false,saver)
+fun rememberSaveableBoolState(initial:Boolean = false,saver: Saver<Boolean, out Any> = autoSaver()) = rememberSaveableState(false,saver)
 @Composable
-inline fun rememberSaveableStringState(initial:String = "",saver: Saver<String, out Any> = autoSaver()) = rememberSaveableState("",saver)
+fun rememberSaveableStringState(initial:String = "",saver: Saver<String, out Any> = autoSaver()) = rememberSaveableState("",saver)
 @Composable
-inline fun rememberSaveableIntState(initial:Int = 0,saver: Saver<Int, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableIntStateOf(initial) }
+fun rememberSaveableIntState(initial:Int = 0,saver: Saver<Int, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableIntStateOf(initial) }
 @Composable
-inline fun rememberSaveableFloatState(initial:Float = 0f,saver: Saver<Float, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableFloatStateOf(initial) }
+fun rememberSaveableFloatState(initial:Float = 0f,saver: Saver<Float, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableFloatStateOf(initial) }
 @Composable
-inline fun rememberSaveableDoubleState(initial: Double = 0.0,saver: Saver<Double, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableDoubleStateOf(initial) }
+fun rememberSaveableDoubleState(initial: Double = 0.0,saver: Saver<Double, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableDoubleStateOf(initial) }
 @Composable
-inline fun rememberSaveableLongState(initial: Long = 0,saver: Saver<Long, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableLongStateOf(initial) }
+fun rememberSaveableLongState(initial: Long = 0,saver: Saver<Long, out Any> = autoSaver()) = rememberSaveable(stateSaver = saver) { mutableLongStateOf(initial) }
 
--- compose-plus/functions/src/commonMain/kotlin/ir/ehsannarmani/compose_plus/functions/state/State.kt
+++ compose-plus/functions/src/commonMain/kotlin/ir/ehsannarmani/compose_plus/functions/state/State.kt
@@ -11,20 +11,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
 @Composable
-inline fun <T>rememberState(value:T) = remember {
+fun <T>rememberState(value:T) = remember {
     mutableStateOf(value)
 }
 @Composable
-inline fun <T>rememberState(value:T,vararg keys:Any) = remember(*keys) {
+fun <T>rememberState(value:T,vararg keys:Any) = remember(*keys) {
     mutableStateOf(value)
 }
 
 @Composable
-inline fun <T>rememberListState(vararg items:T) = remember {
+fun <T>rememberListState(vararg items:T) = remember {
     mutableStateListOf(*items)
 }
 @Composable
-inline fun <reified T>rememberListState(vararg items:T,keys:List<Any> = emptyList()) = remember(*keys.toTypedArray()) {
+fun <T>rememberListState(vararg items:T,keys:List<Any> = emptyList()) = remember(*keys.toTypedArray()) {
     mutableStateListOf(*items)
 }
 @Composable
@@ -33,23 +33,23 @@ inline fun <reified T>rememberListState(items:List<T>) = rememberListState(*item
 inline fun <reified T>rememberListState(items:List<T>,vararg keys: Any) = rememberListState(*items.toTypedArray(),keys)
 
 @Composable
-inline fun <K,V>rememberMapState(vararg pairs:Pair<K,V>) = remember {
+fun <K,V>rememberMapState(vararg pairs:Pair<K,V>) = remember {
     mutableStateMapOf(*pairs)
 }
 @Composable
-inline fun <K,V>rememberMapState(vararg pairs:Pair<K,V>,keys: List<Any>) = remember(*keys.toTypedArray()) {
+fun <K,V>rememberMapState(vararg pairs:Pair<K,V>,keys: List<Any>) = remember(*keys.toTypedArray()) {
     mutableStateMapOf(*pairs)
 }
 
 @Composable
-inline fun rememberBoolState(initial:Boolean = false) = rememberState(false)
+fun rememberBoolState(initial:Boolean = false) = rememberState(false)
 @Composable
-inline fun rememberStringState(initial:String = "") = rememberState("")
+fun rememberStringState(initial:String = "") = rememberState("")
 @Composable
-inline fun rememberIntState(initial:Int = 0) = remember { mutableIntStateOf(initial) }
+fun rememberIntState(initial:Int = 0) = remember { mutableIntStateOf(initial) }
 @Composable
-inline fun rememberFloatState(initial:Float = 0f) = remember { mutableFloatStateOf(initial) }
+fun rememberFloatState(initial:Float = 0f) = remember { mutableFloatStateOf(initial) }
 @Composable
-inline fun rememberDoubleState(initial: Double = 0.0) = remember { mutableDoubleStateOf(initial) }
+fun rememberDoubleState(initial: Double = 0.0) = remember { mutableDoubleStateOf(initial) }
 @Composable
-inline fun rememberLongState(initial: Long = 0) = remember { mutableLongStateOf(initial) }
+fun rememberLongState(initial: Long = 0) = remember { mutableLongStateOf(initial) }
