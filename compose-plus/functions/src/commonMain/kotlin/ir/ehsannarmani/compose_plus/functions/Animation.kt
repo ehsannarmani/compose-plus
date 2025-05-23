b8c2c14a50c57414277555267fbf23ed5cb9dd42
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2025-05-24 01:09:58 +0330

    add extra animation functions

--- compose-plus/functions/src/commonMain/kotlin/ir/ehsannarmani/compose_plus/functions/Animation.kt
+++ compose-plus/functions/src/commonMain/kotlin/ir/ehsannarmani/compose_plus/functions/Animation.kt
@@ -1,11 +1,38 @@
 package ir.ehsannarmani.compose_plus.functions
 
 import androidx.compose.animation.Animatable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun rememberAnimatable(initial:Float = 0f) = remember { androidx.compose.animation.core.Animatable(initial) }
+fun rememberAnimatable(initial: Float = 0f) =
+    remember { androidx.compose.animation.core.Animatable(initial) }
+
 @Composable
 fun rememberAnimatable(initial: Color) = remember { Animatable(initial) }
+
+@Composable
+fun animateFloat(
+    condition: Boolean,
+    first: Float = 1f,
+    second: Float = 0f,
+    animationSpec: AnimationSpec<Float> = tween(400),
+): State<Float> {
+    return animateFloatAsState(if (condition) first else second, animationSpec = animationSpec)
+}
+
+@Composable
+fun animateColor(
+    condition: Boolean,
+    first: Color = Color.Transparent,
+    second: Color = Color.Transparent,
+    animationSpec: AnimationSpec<Color> = tween(400),
+): State<Color> {
+    return animateColorAsState(if (condition) first else second, animationSpec = animationSpec)
+}
